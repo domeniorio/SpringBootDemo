bb6379ac1f2362e25621cc048cafd4787ea7196b
Dooowens <domensiorio@gmail.com>
2025-04-18 19:05:15 +0200

    aggiunta docker compose

--- docker/build_images/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-docker build -t mysql ./mysql/
-
-cp -r ../../target springboot/
-docker build -t spring_app ./springboot/
-rm -rf /springboot/target/
-
--- src/main/java/com/webapp/demo/repo/DemoRepository.java
+++ src/main/java/com/webapp/demo/repo/DemoRepository.java
@@ -1,7 +1,6 @@
 package com.webapp.demo.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import com.webapp.demo.Product;
 
